1e7d526a8319a24a24147826a889f74c9e382144
mmamongy <mariummamdouh@gmail.com>
2018-05-27 09:25:51 +0200

    adding style to the applicatio using ratchet lib

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 var Header = {
 	view: function(ctrl, args) {
-		return m('h1.title', args.text);
+		return m('header.bar.bar-nav',[
+			m('a', {
+				href: window.location.pathname,
+				class: 'icon icon-left-nav pull-left' + (args.back ? '' : 'hidden')
+			}),
+			m('h1.title', args.text)
+		])
+		
 	}
 };
 
@@ -15,23 +22,33 @@ var SearchBar = {
 		};
 	},
 	view: function(ctrl) {
-		return m('input[type=search]', {
+		return m('.bar.bar-standard.bar-header-secondary',[
+
+		
+		m('input[type=search]', {
 			value: ctrl.searchKey(),
 			oninput: ctrl.searchHandler
-		});
+		})
+	]) 
 	}
 };
 
 var EmployeeListItem = {
 	view: function(ctrl, args) {
-		return m('li', [
+		return m('li.table-view-cell.media',[
 			m('a', {
+			
 				href: window.location.href + 'employees/' + args.employee.id
 			}, [
+				m('img.media-object.small.pull-left',{
+					src: 'pics/' + args.employee.firstName+'_'+args.employee.lastName+'.jpg'
+				}),
 				m('span', args.employee.firstName),
-				m('span', args.employee.lastName)
-			])
+				m('span', ' '),
+				m('span', args.employee.lastName),
+				m('p', args.employee.title)
 		])
+	])
 	}
 };
 
@@ -81,15 +98,66 @@ var EmployeePage =  {
 		})
 	},
 	view: function( ctrl, args){
-		return ('div',[
-			m.component(Header,{ text: 'Employee Details'}),
-			m('h3', ctrl.employee().firstName+ ' '  + ctrl.employee().lastName),
-			ctrl.employee().title
-			
+		return m('div', [
+			m.component(Header, {
+				text: 'Employee Details',
+				back: true
+			}),
+			m('.card', [
+				m('ul.table-view', [
+					m('li.table-view-cell.media', [
+						m('img.media-object.big.pull-left', {
+							src: 'pics/' + ctrl.employee().firstName+'_'+ctrl.employee().lastName + '.jpg'
+						}),
+						m('h1', [
+							m('span', ctrl.employee().firstName),
+							m('span', ' '),
+							m('span', ctrl.employee().lastName)
+						]),
+						m('p', ctrl.employee().title)
+					]),
+					m('li.table-view-cell.media', [
+						m('a.push-right', {
+							href: 'tel:' + ctrl.employee().officePhone
+						}, [
+							m('span.media-object.pull-left.icon.icon-call'),
+							m('.media-body', 'Call Office', [
+								m('p', ctrl.employee().officePhone)
+							])
+						])
+					]),
+					m('li.table-view-cell.media', [
+						m('a.push-right', {
+							href: 'sms:' + ctrl.employee().mobilePhone
+						}, [
+							m('span.media-object.pull-left.icon.icon-sms'),
+							m('.media-body', 'SMS', [
+								m('p', ctrl.employee().mobilePhone)
+							])
+						])
+					]),
+					m('li.table-view-cell.media', [
+						m('a.push-right', {
+							href: 'mailto:' + ctrl.employee().email
+						}, [
+							m('span.media-object.pull-left.icon.icon-email'),
+							m('.media-body', 'Email', [
+								m('p', ctrl.employee().email)
+							])
+						])
+					])
+				])
+			])
 		])
 	}
-} ;
 
+		// return ('div',[
+		// 	m.component(Header,{ text: 'Employee Details'}),
+		// 	m('h3', ctrl.employee().firstName+ ' '  + ctrl.employee().lastName),
+		// 	ctrl.employee().title
+			
+		// ])
+	};
 m.route(document.body, '/',{
 	'/': m.component(HomePage,{service: employeeService}),
 	'/employees/:Id': m.component(EmployeePage, {service: employeeService})
